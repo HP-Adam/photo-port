a95618550eeb4ee4a9f847d1c712f55090a6a8d0
HP-Adam <adamhpeterman@gmail.com>
2021-08-01 12:12:00 -0500

    open modal with chosen picture

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Modal from "../Modal";
 
 const PhotoList = ({ category }) => {
   const [photos] = useState([
@@ -120,8 +121,17 @@ const PhotoList = ({ category }) => {
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
+  const [currentPhoto, setCurrentPhoto] = useState();
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
@@ -129,6 +139,10 @@ const PhotoList = ({ category }) => {
             alt={image.name}
             className="img-thumbnail mx-1"
             key={image.name}
+            onClick={() => {
+              toggleModal(image, i);
+              setIsModalOpen(true);
+            }}
           />
         ))}
       </div>
